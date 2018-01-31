8d597bd76f6c32a825f64b56ec7a0f8e8fac031a
canopy <visvanathannaditya@gmail.com>
2018-01-31 22:25:32 +0530

    test cli argument parsing

--- src/main.c
+++ src/main.c
@@ -13,39 +13,41 @@
 #include <locale.h>
 int main(int argc,char **argv)
 {
-    WINDOW *main_win;
+//    WINDOW *main_win;
     /* set locale to C to speed up performance */
-    setlocale(LC_ALL,"C");
+//    setlocale(LC_ALL,"C");
 
     /* check if repo exists */
-    if ( ! check_if_repo() )
+    char *dir = argv[0];
+    printf("Dir:%s\n",dir);
+    /*if ( ! check_if_repo() )
     {
         printf("gitc : Fatal! Not a Git Repository!\n");
         return E_EXIT;
     }
-    
+    */
     /* initialize curses */
-    main_win = initscr();
+    /*main_win = initscr();
     curs_set(0);
     cbreak();
     keypad(main_win,TRUE);
     noecho();
-    
+    */
     /* exit main app if user triggers exit at welcome screen */
-    if( print_welc_scr(main_win) == E_EXIT )
+    /*if( print_welc_scr(main_win) == E_EXIT )
     {
         curs_set(1);
         nocbreak();
         endwin();
         return E_EXIT;
-    }
+    }*/
     /* continue with main app after main screen if exit is not triggered */
-    repo_commit_menu(main_win);   
+   // repo_commit_menu(main_win);   
 
     /* clean up and exit curses on main app exit */  
-    curs_set(1); 
-    nocbreak();
-    endwin();
-
+   // curs_set(1); 
+   // nocbreak();
+   // endwin();
+   // */
     return E_SUCCESS;
 }
